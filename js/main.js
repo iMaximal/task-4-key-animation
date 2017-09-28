965a15ba5da641cb1c4f09c97667457b212cf9c3
Maximal <maxim@infofree.ru>
2017-09-29 00:50:00 +0500

    Finish project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.body.addEventListener('keypress', keyboardHandler);
+
+function keyboardHandler(event) {
+
+    var charCode = event.which || event.keyCode;
+    console.log(charCode);
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || // english
+        (charCode > 1024 && charCode < 1106)) { // russian
+        var divElement = document.createElement('div');
+        divElement.className = 'fade-in';
+        divElement.textContent = String.fromCharCode(charCode);
+
+        var container = document.querySelector('.container');
+        container.appendChild(divElement);
+    }
+
+}
+
